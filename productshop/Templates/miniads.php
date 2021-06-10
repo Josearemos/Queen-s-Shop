b8428395e9de6f91b97ac2cceaf22052ecd0bdce
Josearemos <josearemos01@gmail.com>
2021-06-10 17:22:49 +0200

    delete

--- productshop/Templates/miniads.php
+++
@@ -1,7 +0,0 @@
-<!-- Banners Anuncios  -->
-<section id="banner_adds">
-    <div class="container py-5 text-center">
-        <img src="./assets/Banner4.png" alt="banner1" class="img-fluid">
-        <img src="./assets/Banner5.png" alt="banner1" class="img-fluid">
-    </div>
-</section>
